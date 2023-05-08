1dd84650c5889be4702091cdad6f0d2ca06aa49f
Akim <akim.fuzhenko@gmail.com>
2023-05-08 14:36:07 +0300

    catalog-tabs, need to fix switching tab function

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-    $('.carousel__inner').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 700,
-        adaptiveHeight: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/left.png"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/right.png"</button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false
-              }
-            }
-        ]
-    });
-})
+	$('.carousel__inner').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 700,
+		adaptiveHeight: false,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/right.png"></button>',
+		responsive: [
+			{
+			  breakpoint: 992,
+			  settings: {
+				dots: true,
+				arrows: false
+			  }
+			}
+		]
+	});
+
+	// activeToTab
+
+	let tabsButtons = document.querySelectorAll('.catalog__tab');
+	let activeClass = 'catalog__tab' + '_active';
+	let catalogItem = document.querySelector('.catalog__content');
+	function addActiveToTab (tabs, active) {
+		tabs.forEach(item => {
+			item.addEventListener('click', () => {
+				removeActiveToTabs(tabs, active);
+				item.classList.add(activeClass);
+
+				catalogItem.classList.add('catalog__content_active');
+			});
+			
+		});
+	}
+
+	function removeActiveToTabs (tabs, active) {
+		tabs.forEach(item => {
+			item.classList.remove(active);
+		}); 
+	}
+
+	addActiveToTab(tabsButtons, activeClass);
+
+	// activeToContent
+
+	let catalog__content = document.querySelectorAll('.catalog-item__content'),
+		catalog__list = document.querySelectorAll('.catalog-item__list'),
+		catalog__button = document.querySelectorAll('.catalog-item a'),
+		activeContent = 'catalog-item__content_active',
+		activeList = 'catalog-item__list_active';
+
+	function toggleActiveTabsContent (i) {
+		catalog__content[i].classList.toggle(activeContent);
+		catalog__list[i].classList.toggle(activeList);
+	}
+
+	catalog__button.forEach((item, i) => {
+		item.addEventListener('click', () => {
+			toggleActiveTabsContent(Math.floor(i/2)); // как это работает: нужно делить нацело на 2 чтобы оно считало попорядку кнопки, иначе оно скачет
+		});
+	});
+});
